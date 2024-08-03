866e65024e3b397c6ada2e4ac220b62cb5832636
mahircihangirsarac <mahircihangirsarac@gmail.com>
2024-08-03 19:41:24 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TRIZMatrix from "./TRIZMatrix.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <TRIZMatrix />
+    </div>
+  );
+}
+
+export default App;
+
